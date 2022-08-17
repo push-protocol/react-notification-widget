b6928792766939674de0e4b67fc0f41493c7d346
Tornike Mamatsashvili <tornike.mamatsashvili.1@gmail.com>
2022-08-18 02:19:30 +0400

    clean up unused configs and files

--- esbuild-serve.js
+++ esbuild-serve.js
@@ -1,24 +1,14 @@
-const esbuild = require('esbuild');
-const inlineImage = require('esbuild-plugin-inline-image');
-const alias = require('esbuild-plugin-alias');
+import esbuildServe from 'esbuild-serve';
 
-esbuild
-  .serve(
-    {
-      servedir: 'public',
-      port: 3000,
-    },
-    {
-      entryPoints: ['./src/dev.tsx'],
-      outfile: './public/dist/app.js',
-      bundle: true,
-      loader: {
-        '.js': 'jsx',
-      },
-      plugins: [inlineImage()],
-      define: {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      },
-    }
-  )
-  .catch(() => process.exit(1));
+esbuildServe({
+  entryPoints: ['./src/dev.tsx'],
+  outfile: './public/dist/app.js',
+  bundle: true,
+  sourcemap: true,
+  loader: {
+    '.js': 'jsx',
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  },
+});
--- esbuild.config.js
+++ esbuild.config.js
@@ -1,15 +1,33 @@
-import esbuildServe from 'esbuild-serve';
+import { createRequire } from 'module';
+import { build } from 'esbuild';
 
-esbuildServe(
-  {
-    logLevel: 'info',
-    entryPoints: ['src/dev.tsx'],
-    bundle: true,
-    outfile: 'public/dist/app.js',
-    sourcemap: true,
-    define: {
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-    },
+const require = createRequire(import.meta.url);
+const { dependencies } = require('./package.json');
+
+const entryFile = 'src/index.tsx';
+
+const shared = {
+  bundle: true,
+  entryPoints: [entryFile],
+  external: Object.keys(dependencies),
+  logLevel: 'info',
+  minify: true,
+  sourcemap: true,
+  loader: {
+    '.js': 'jsx',
   },
-  { root: 'public' }
-);
+};
+
+build({
+  ...shared,
+  format: 'esm',
+  outfile: './dist/index.esm.js',
+  target: ['esnext', 'node12.22.0'],
+}).catch(() => process.exit(1));
+
+build({
+  ...shared,
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
+  target: ['esnext', 'node12.22.0'],
+}).catch(() => process.exit(1));
