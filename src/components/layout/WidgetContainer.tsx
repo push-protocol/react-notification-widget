0fd37e87b599b7be9d8a91bfbec35ad5fbeea0b4
TornikeW <112381628+TornikeW@users.noreply.github.com>
2022-10-03 18:09:44 +0400

    Link powered by wherever to a faq page of wherever website (#13)

--- src/components/layout/WidgetContainer.tsx
+++ src/components/layout/WidgetContainer.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 import Flex from './Flex';
 import Text from 'components/Text';
+import Link from 'components/Link';
 
 const POWERED_BY_HEIGHT = '20px';
 
@@ -50,9 +51,11 @@ export const WidgetContainer = ({ children }: LayoutProps) => {
         <Text size={'sm'} color={'secondary'} opacity={0.8} weight={500}>
           Powered by
         </Text>
-        <Text size={'sm'} opacity={0.8} weight={600} ml={0.4} color={'secondary'}>
-          Wherever
-        </Text>
+        <Link src={'https://faq.wherever.to'}>
+          <Text size={'sm'} opacity={0.8} weight={600} ml={0.4} color={'secondary'}>
+            Wherever
+          </Text>
+        </Link>
       </PoweredBy>
     </LayoutContainer>
   );
