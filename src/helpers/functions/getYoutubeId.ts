6d61992e1eb8f9bcb668bc658da9411cd0e1cb78
Tornike Mamatsashvili <tornike@wherever.to>
2022-10-05 21:58:52 +0400

    Support media files on widget

---
+++ src/helpers/functions/getYoutubeId.ts
@@ -0,0 +1,6 @@
+export const getYoutubeId = (url?: string) => {
+  if (!url) return false;
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[7].length == 11 ? match[7] : false;
+};
