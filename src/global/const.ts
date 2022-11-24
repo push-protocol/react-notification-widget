9fa08aa438fc0f803614a18a6fd347696f311762
nivo33 <niv@wherever.to>
2022-11-24 15:14:46 +0200

    Fix types location in package.json

--- src/global/const.ts
+++ src/global/const.ts
@@ -1,3 +1,5 @@
+// because package.json is outside "src", this import changes the way the types are output to the dist folder
+// if this import is removed, the "types" in package.json should be changed
 import pkg from '../../package.json';
 
 export const LOCALSTORAGE_AUTH_KEY = 'whereverAuthToken';
