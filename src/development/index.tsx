2f8e463789ec9d85621e13317cc3f578a45ad294
Tornike <tornike@wherever.to>
2022-12-08 13:49:45 +0400

    Change widget to default mode

--- src/development/index.tsx
+++ src/development/index.tsx
@@ -48,11 +48,7 @@ const FakeApp = () => {
 
   const widget = useMemo(() => {
     return (
-      <NotificationFeedProvider
-        theme={theme}
-        partnerKey={partnerKey}
-        mode={WidgetMode.SubscribeOnly}
-      >
+      <NotificationFeedProvider theme={theme} partnerKey={partnerKey} mode={WidgetMode.Default}>
         <NotificationFeed gapFromBell={10}>
           <S.WidgetBellWrapper>
             <NotificationBell />
