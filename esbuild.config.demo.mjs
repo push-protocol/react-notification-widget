21a03e8de4a634c9a549264b0e8716f9e7d6c17d
nivo33 <niv@wherever.to>
2023-01-01 13:35:36 +0200

    set to staging env

--- esbuild.config.demo.mjs
+++ esbuild.config.demo.mjs
@@ -18,7 +18,7 @@ esbuildServe({
     })
   ],
   define: {
-    'process.env.WHEREVER_ENV': JSON.stringify('production'),
+    'process.env.WHEREVER_ENV': JSON.stringify('staging'),
     'process.env.NODE_ENV': JSON.stringify('production'), // required for bundling
     global: 'window',
   },
