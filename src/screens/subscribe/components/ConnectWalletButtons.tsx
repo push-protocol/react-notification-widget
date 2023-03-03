aa6447907377e62f2aac09a8ec0b4dd7cf6ae38b
robert <4r.iskey@gmail.com>
2023-03-03 12:15:54 +0400

    Fixed issue

--- src/screens/subscribe/components/ConnectWalletButtons.tsx
+++ src/screens/subscribe/components/ConnectWalletButtons.tsx
@@ -3,11 +3,13 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import styled from 'styled-components';
 import analytics from 'services/analytics';
 import Flex from 'components/layout/Flex';
+import Text from 'components/Text';
 
 const Container = styled(Flex)`
   * {
     width: 100%;
   }
+
   button {
     display: flex;
     justify-content: center;
@@ -28,6 +30,9 @@ const ConnectWalletButtons = () => {
       direction={'column'}
       gap={1.5}
     >
+      <Text mb={1} size={'sm'} align={'center'}>
+        <strong>Connect your wallet to continue</strong>
+      </Text>
       <ConnectButton label={'Connect wallet'} />
     </Container>
   );
