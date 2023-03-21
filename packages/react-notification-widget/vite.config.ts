0f176d5659d5ae17fc4c5bf8aa2d243a4c0a3956
robert <4r.iskey@gmail.com>
2023-03-21 17:58:46 +0400

    Fixed pths

--- packages/react-notification-widget/vite.config.ts
+++ packages/react-notification-widget/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       entryRoot: 'src',
       tsConfigFilePath: join(__dirname, 'tsconfig.lib.json'),
       skipDiagnostics: true,
+      outputDir: '../../dist/packages/react-notification-widget/types',
     }),
     react(),
     viteTsConfigPaths({
@@ -30,7 +31,7 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
       name: 'Wherever Notification Widget',
-      fileName: 'index',
+      fileName: (format) => `${format}/index.js`,
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
       formats: ['es', 'cjs'],
