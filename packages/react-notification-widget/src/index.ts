168176637845f4680a7538bdaf12a8b6cb4776ab
robert <4r.iskey@gmail.com>
2023-03-27 16:30:49 +0400

    - Fixed unauthentication issue - Fixed long ens name

--- packages/react-notification-widget/src/index.ts
+++ packages/react-notification-widget/src/index.ts
@@ -3,10 +3,10 @@ import NotificationFeedProvider, {
   NotificationFeedProviderProps,
 } from './components/NotificationFeedProvider';
 import { ThemeMode, type CustomTheme } from './theme';
-import type { Notification } from './context/UserContext/types';
+import type { Notification } from 'context/UserContext/types';
 import NotificationFeed, { NotificationFeedProps } from './components/NotificationFeed';
-import { WidgetMode } from './context/EnvironmentContext';
-import type { CustomSigner } from './context/SignerContext';
+import { WidgetMode } from 'context/EnvironmentContext';
+import type { CustomSigner } from 'context/SignerContext';
 
 export { NotificationFeed, NotificationFeedProvider, NotificationBell, ThemeMode, WidgetMode };
 
